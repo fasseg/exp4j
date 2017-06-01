81f91eedbde865e5fb4d2ee00405877fead0eff2
leogtzr <leorocko13@hotmail.com>
2017-06-01 14:23:51 -0600

    Cleanup ...

--- src/test/java/net/objecthunter/exp4j/ExpressionTest.java
+++ src/test/java/net/objecthunter/exp4j/ExpressionTest.java
@@ -34,7 +34,7 @@ import java.util.concurrent.Executors;
 
 public class ExpressionTest {
     @Test
-    public void testExpression1() throws Exception{
+    public void testExpression1() {
         Token[] tokens = new Token[] {
             new NumberToken(3d),
             new NumberToken(2d),
@@ -45,7 +45,7 @@ public class ExpressionTest {
     }
 
     @Test
-    public void testExpression2() throws Exception{
+    public void testExpression2() {
         Token[] tokens = new Token[] {
                 new NumberToken(1d),
                 new FunctionToken(Functions.getBuiltinFunction("log")),
@@ -55,7 +55,7 @@ public class ExpressionTest {
     }
 
     @Test
-    public void testGetVariableNames1() throws Exception{
+    public void testGetVariableNames1() {
         Token[] tokens = new Token[] {
                 new VariableToken("a"),
                 new VariableToken("b"),
@@ -67,7 +67,7 @@ public class ExpressionTest {
     }
 
     @Test
-    public void testFactorial() throws Exception {
+    public void testFactorial() {
         Operator factorial = new Operator("!", 1, true, Precedence.POWER + 1) {
 
             @Override
@@ -228,7 +228,7 @@ public class ExpressionTest {
     @Test
     @Ignore
     // If Expression should be threads safe this test must pass
-    public void evaluateFamily() throws Exception {
+    public void evaluateFamily() {
         final Expression e = new ExpressionBuilder("sin(x)")
                 .variable("x")
                 .build();
