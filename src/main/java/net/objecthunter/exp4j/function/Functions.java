c3227c813428601ebff8b5e1bb0daaae6b28b1c5
leogtzr <leorocko13@hotmail.com>
2017-06-01 11:34:12 -0600

    Changing builtinFunctions class variable name to comply with constants naming conventions.

--- src/main/java/net/objecthunter/exp4j/function/Functions.java
+++ src/main/java/net/objecthunter/exp4j/function/Functions.java
@@ -19,6 +19,7 @@ package net.objecthunter.exp4j.function;
  * Class representing the builtin functions available for use in expressions
  */
 public class Functions {
+    
     private static final int INDEX_SIN = 0;
     private static final int INDEX_COS = 1;
     private static final int INDEX_TAN = 2;
@@ -43,28 +44,28 @@ public class Functions {
     private static final int INDEX_LOG2 = 21;
     private static final int INDEX_SGN = 22;
 
-    private static final Function[] builtinFunctions = new Function[23];
+    private static final Function[] BUILTIN_FUNCTIONS = new Function[23];
 
     static {
-        builtinFunctions[INDEX_SIN] = new Function("sin") {
+        BUILTIN_FUNCTIONS[INDEX_SIN] = new Function("sin") {
             @Override
             public double apply(double... args) {
                 return Math.sin(args[0]);
             }
         };
-        builtinFunctions[INDEX_COS] = new Function("cos") {
+        BUILTIN_FUNCTIONS[INDEX_COS] = new Function("cos") {
             @Override
             public double apply(double... args) {
                 return Math.cos(args[0]);
             }
         };
-        builtinFunctions[INDEX_TAN] = new Function("tan") {
+        BUILTIN_FUNCTIONS[INDEX_TAN] = new Function("tan") {
             @Override
             public double apply(double... args) {
                 return Math.tan(args[0]);
             }
         };
-        builtinFunctions[INDEX_COT] = new Function("cot") {
+        BUILTIN_FUNCTIONS[INDEX_COT] = new Function("cot") {
             @Override
             public double apply(double... args) {
                 double tan = Math.tan(args[0]);
@@ -74,115 +75,115 @@ public class Functions {
                 return 1d/Math.tan(args[0]);
             }
         };
-        builtinFunctions[INDEX_LOG] = new Function("log") {
+        BUILTIN_FUNCTIONS[INDEX_LOG] = new Function("log") {
             @Override
             public double apply(double... args) {
                 return Math.log(args[0]);
             }
         };
-        builtinFunctions[INDEX_LOG2] = new Function("log2") {
+        BUILTIN_FUNCTIONS[INDEX_LOG2] = new Function("log2") {
             @Override
             public double apply(double... args) {
                 return Math.log(args[0]) / Math.log(2d);
             }
         };
-        builtinFunctions[INDEX_LOG10] = new Function("log10") {
+        BUILTIN_FUNCTIONS[INDEX_LOG10] = new Function("log10") {
             @Override
             public double apply(double... args) {
                 return Math.log10(args[0]);
             }
         };
-        builtinFunctions[INDEX_LOG1P] = new Function("log1p") {
+        BUILTIN_FUNCTIONS[INDEX_LOG1P] = new Function("log1p") {
             @Override
             public double apply(double... args) {
                 return Math.log1p(args[0]);
             }
         };
-        builtinFunctions[INDEX_ABS] = new Function("abs") {
+        BUILTIN_FUNCTIONS[INDEX_ABS] = new Function("abs") {
             @Override
             public double apply(double... args) {
                 return Math.abs(args[0]);
             }
         };
-        builtinFunctions[INDEX_ACOS] = new Function("acos") {
+        BUILTIN_FUNCTIONS[INDEX_ACOS] = new Function("acos") {
             @Override
             public double apply(double... args) {
                 return Math.acos(args[0]);
             }
         };
-        builtinFunctions[INDEX_ASIN] = new Function("asin") {
+        BUILTIN_FUNCTIONS[INDEX_ASIN] = new Function("asin") {
             @Override
             public double apply(double... args) {
                 return Math.asin(args[0]);
             }
         };
-        builtinFunctions[INDEX_ATAN] = new Function("atan") {
+        BUILTIN_FUNCTIONS[INDEX_ATAN] = new Function("atan") {
             @Override
             public double apply(double... args) {
                 return Math.atan(args[0]);
             }
         };
-        builtinFunctions[INDEX_CBRT] = new Function("cbrt") {
+        BUILTIN_FUNCTIONS[INDEX_CBRT] = new Function("cbrt") {
             @Override
             public double apply(double... args) {
                 return Math.cbrt(args[0]);
             }
         };
-        builtinFunctions[INDEX_FLOOR] = new Function("floor") {
+        BUILTIN_FUNCTIONS[INDEX_FLOOR] = new Function("floor") {
             @Override
             public double apply(double... args) {
                 return Math.floor(args[0]);
             }
         };
-        builtinFunctions[INDEX_SINH] = new Function("sinh") {
+        BUILTIN_FUNCTIONS[INDEX_SINH] = new Function("sinh") {
             @Override
             public double apply(double... args) {
                 return Math.sinh(args[0]);
             }
         };
-        builtinFunctions[INDEX_SQRT] = new Function("sqrt") {
+        BUILTIN_FUNCTIONS[INDEX_SQRT] = new Function("sqrt") {
             @Override
             public double apply(double... args) {
                 return Math.sqrt(args[0]);
             }
         };
-        builtinFunctions[INDEX_TANH] = new Function("tanh") {
+        BUILTIN_FUNCTIONS[INDEX_TANH] = new Function("tanh") {
             @Override
             public double apply(double... args) {
                 return Math.tanh(args[0]);
             }
         };
-        builtinFunctions[INDEX_COSH] = new Function("cosh") {
+        BUILTIN_FUNCTIONS[INDEX_COSH] = new Function("cosh") {
             @Override
             public double apply(double... args) {
                 return Math.cosh(args[0]);
             }
         };
-        builtinFunctions[INDEX_CEIL] = new Function("ceil") {
+        BUILTIN_FUNCTIONS[INDEX_CEIL] = new Function("ceil") {
             @Override
             public double apply(double... args) {
                 return Math.ceil(args[0]);
             }
         };
-        builtinFunctions[INDEX_POW] = new Function("pow", 2) {
+        BUILTIN_FUNCTIONS[INDEX_POW] = new Function("pow", 2) {
             @Override
             public double apply(double... args) {
                 return Math.pow(args[0], args[1]);
             }
         };
-        builtinFunctions[INDEX_EXP] = new Function("exp", 1) {
+        BUILTIN_FUNCTIONS[INDEX_EXP] = new Function("exp", 1) {
             @Override
             public double apply(double... args) {
                 return Math.exp(args[0]);
             }
         };
-        builtinFunctions[INDEX_EXPM1] = new Function("expm1", 1) {
+        BUILTIN_FUNCTIONS[INDEX_EXPM1] = new Function("expm1", 1) {
             @Override
             public double apply(double... args) {
                 return Math.expm1(args[0]);
             }
         };
-        builtinFunctions[INDEX_SGN] = new Function("signum", 1) {
+        BUILTIN_FUNCTIONS[INDEX_SGN] = new Function("signum", 1) {
             @Override
             public double apply(double... args) {
                 if (args[0] > 0) {
@@ -204,51 +205,51 @@ public class Functions {
     public static Function getBuiltinFunction(final String name) {
 
         if (name.equals("sin")) {
-            return builtinFunctions[INDEX_SIN];
+            return BUILTIN_FUNCTIONS[INDEX_SIN];
         } else if (name.equals("cos")) {
-            return builtinFunctions[INDEX_COS];
+            return BUILTIN_FUNCTIONS[INDEX_COS];
         } else if (name.equals("tan")) {
-            return builtinFunctions[INDEX_TAN];
+            return BUILTIN_FUNCTIONS[INDEX_TAN];
         } else if (name.equals("cot")) {
-            return builtinFunctions[INDEX_COT];
+            return BUILTIN_FUNCTIONS[INDEX_COT];
         } else if (name.equals("asin")) {
-            return builtinFunctions[INDEX_ASIN];
+            return BUILTIN_FUNCTIONS[INDEX_ASIN];
         } else if (name.equals("acos")) {
-            return builtinFunctions[INDEX_ACOS];
+            return BUILTIN_FUNCTIONS[INDEX_ACOS];
         } else if (name.equals("atan")) {
-            return builtinFunctions[INDEX_ATAN];
+            return BUILTIN_FUNCTIONS[INDEX_ATAN];
         } else if (name.equals("sinh")) {
-            return builtinFunctions[INDEX_SINH];
+            return BUILTIN_FUNCTIONS[INDEX_SINH];
         } else if (name.equals("cosh")) {
-            return builtinFunctions[INDEX_COSH];
+            return BUILTIN_FUNCTIONS[INDEX_COSH];
         } else if (name.equals("tanh")) {
-            return builtinFunctions[INDEX_TANH];
+            return BUILTIN_FUNCTIONS[INDEX_TANH];
         } else if (name.equals("abs")) {
-            return builtinFunctions[INDEX_ABS];
+            return BUILTIN_FUNCTIONS[INDEX_ABS];
         } else if (name.equals("log")) {
-            return builtinFunctions[INDEX_LOG];
+            return BUILTIN_FUNCTIONS[INDEX_LOG];
         } else if (name.equals("log10")) {
-            return builtinFunctions[INDEX_LOG10];
+            return BUILTIN_FUNCTIONS[INDEX_LOG10];
         } else if (name.equals("log2")) {
-            return builtinFunctions[INDEX_LOG2];
+            return BUILTIN_FUNCTIONS[INDEX_LOG2];
         } else if (name.equals("log1p")) {
-            return builtinFunctions[INDEX_LOG1P];
+            return BUILTIN_FUNCTIONS[INDEX_LOG1P];
         } else if (name.equals("ceil")) {
-            return builtinFunctions[INDEX_CEIL];
+            return BUILTIN_FUNCTIONS[INDEX_CEIL];
         } else if (name.equals("floor")) {
-            return builtinFunctions[INDEX_FLOOR];
+            return BUILTIN_FUNCTIONS[INDEX_FLOOR];
         } else if (name.equals("sqrt")) {
-            return builtinFunctions[INDEX_SQRT];
+            return BUILTIN_FUNCTIONS[INDEX_SQRT];
         } else if (name.equals("cbrt")) {
-            return builtinFunctions[INDEX_CBRT];
+            return BUILTIN_FUNCTIONS[INDEX_CBRT];
         } else if (name.equals("pow")) {
-            return builtinFunctions[INDEX_POW];
+            return BUILTIN_FUNCTIONS[INDEX_POW];
         } else if (name.equals("exp")) {
-            return builtinFunctions[INDEX_EXP];
+            return BUILTIN_FUNCTIONS[INDEX_EXP];
         } else if (name.equals("expm1")) {
-            return builtinFunctions[INDEX_EXPM1];
+            return BUILTIN_FUNCTIONS[INDEX_EXPM1];
         } else if (name.equals("signum")) {
-            return builtinFunctions[INDEX_SGN];
+            return BUILTIN_FUNCTIONS[INDEX_SGN];
         } else {
             return null;
         }
