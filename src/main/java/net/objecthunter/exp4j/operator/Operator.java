cd44c67486f74a0e7099c68909788df4273ce231
7rans <transfire@gmail.com>
2018-05-18 22:04:07 -0400

    Add × and ÷ as valid operators

--- src/main/java/net/objecthunter/exp4j/operator/Operator.java
+++ src/main/java/net/objecthunter/exp4j/operator/Operator.java
@@ -55,7 +55,7 @@ public abstract class Operator {
     /**
      * The set of allowed operator chars
      */
-    public static final char[] ALLOWED_OPERATOR_CHARS = { '+', '-', '*', '/', '%', '^', '!', '#','§', '$', '&', ';', ':', '~', '<', '>', '|', '='};
+    public static final char[] ALLOWED_OPERATOR_CHARS = { '+', '-', '*', '/', '%', '^', '!', '#','§', '$', '&', ';', ':', '~', '<', '>', '|', '=', '×', '÷' };
 
     protected final int numOperands;
     protected final boolean leftAssociative;
