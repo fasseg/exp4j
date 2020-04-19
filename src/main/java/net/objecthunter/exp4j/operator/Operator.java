ed8f4150e63336b11441db5a826cf19aeb632eb5
lyeeedar <lyeeedar@gmail.com>
2020-04-19 17:47:57 +0100

    Remove strange symbol

--- src/main/java/net/objecthunter/exp4j/operator/Operator.java
+++ src/main/java/net/objecthunter/exp4j/operator/Operator.java
@@ -1,4 +1,4 @@
-/* 
+/*
 * Copyright 2014 Frank Asseg
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +11,7 @@
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
-* limitations under the License. 
+* limitations under the License.
 */
 package net.objecthunter.exp4j.operator;
 
@@ -55,7 +55,7 @@ public abstract class Operator {
     /**
      * The set of allowed operator chars
      */
-    public static final char[] ALLOWED_OPERATOR_CHARS = { '+', '-', '*', '/', '%', '^', '!', '#','§', '$', '&', ';', ':', '~', '<', '>', '|', '='};
+    public static final char[] ALLOWED_OPERATOR_CHARS = { '+', '-', '*', '/', '%', '^', '!', '#', '$', '&', ';', ':', '~', '<', '>', '|', '='};
 
     protected final int numOperands;
     protected final boolean leftAssociative;
